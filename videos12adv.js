02c8bdcf50fbeee23750d0308a2a20187406134d
IMoTaha <24motaha@gmail.com>
2024-08-08 19:56:45 +0400

    Update videos12adv.js

--- videos12adv.js
+++ videos12adv.js
@@ -338,3 +338,9 @@ document.onkeydown = (e) => {
         e.preventDefault();
     }
 };
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
