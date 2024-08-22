f06b8d59fbdeda5825f09db3d9ead33d321d3e4d
IMoTaha <24motaha@gmail.com>
2024-08-22 20:56:00 +0400

    Update videos12adv.js

--- videos12adv.js
+++ videos12adv.js
@@ -1,4 +1,4 @@
-const main_video = document.querySelector('.main-video video');
+const main_video = document.getElementById('main-video-container');
 const main_video_title = document.querySelector('.main-video .title');
 const video_playlist = document.querySelector('.video-playlist .videos');
 
@@ -6,230 +6,312 @@ let data = [
     {
         'id': 'a1',
         'title': '1.1 كثيرات الحدود والدوال النسبية',
-        'name': '1.1 كثيرات الحدود والدوال النسبية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution1_1.pdf',
-        'noSolutionLink': 'nosolution1_1.pdf',
-        'powerpointLink': 'powerpoint1_1.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a2',
         'title': '1.2 الدوال العكسية',
-        'name': '1.2 الدوال العكسية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution1_2.pdf',
-        'noSolutionLink': 'nosolution1_2.pdf',
-        'powerpointLink' : 'powerpoint1_2.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a3',
         'title': '1.3 الدوال المثلثية والدوال المثلثية العكسية',
-        'name': '1.3 الدوال المثلثية والدوال المثلثية العكسية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution1_3.pdf',
-        'noSolutionLink': 'nosolution1_3.pdf',
-        'powerpointLink': 'powerpoint1_3.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
 
     {
         'id': 'a4',
         'title': '1.4 الدوال الأسية واللوغاريتمية',
-        'name': '1.4 الدوال الأسية واللوغاريتمية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution1_4.pdf',
-        'noSolutionLink': 'nosolution1_4.pdf',
-        'powerpointLink': 'powerpoint1_4.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a5',
         'title': '1.5 تحويلات الدوال',
-        'name': '1.5 تحويلات الدوال.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution1_5.pdf',
-        'noSolutionLink': 'nosolution1_5.pdf',
-        'powerpointLink': 'powerpoint1_5.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a6',
         'title': '2.1 مراجعة موجزة عن التفاضل والتكامل: المماسات وطول المنحنى',
-        'name': '2.1 مراجعة موجزة عن التفاضل والتكامل: المماسات وطول المنحنى.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution2_1.pdf',
-        'noSolutionLink': 'nosolution2_1.pdf',
-        'powerpointLink': 'powerpoint2_1.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a7',
         'title': '2.2 مفهوم النهاية',
-        'name': '2.2 مفهوم النهاية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution2_2.pdf',
-        'noSolutionLink': 'nosolution2_2.pdf',
-        'powerpointLink': 'powerpoint2_2.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
 
     {
         'id': 'a8',
         'title': '2.3 حساب النهايات',
-        'name': '2.3 حساب النهايات.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution2_3.pdf',
-        'noSolutionLink': 'nosolution2_3.pdf',
-        'powerpointLink': 'powerpoint2_3.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a9',
         'title': '2.4 الاتصال ونتائجه',
-        'name': '2.4 الاتصال ونتائجه.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution2_4.pdf',
-        'noSolutionLink': 'nosolution2_4.pdf',
-        'powerpointLink': 'powerpoint2_4.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a10',
         'title': '2.5 النهايات التي تتضمن اللانهاية: خطوط التقارب',
-        'name': '2.5 النهايات التي تتضمن اللانهاية: خطوط التقارب.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution2_5.pdf',
-        'noSolutionLink': 'nosolution2_5.pdf',
-        'powerpointLink': 'powerpoint2_5.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a11',
         'title': '3.1 المماسات والسرعة المتجهة',
-        'name': '3.1 المماسات والسرعة المتجهة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_1.pdf',
-        'noSolutionLink': 'nosolution3_1.pdf',
-        'powerpointLink': 'powerpoint3_1.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a12',
         'title': '3.2 الاشتقاق',
-        'name': '3.2 الاشتقاق.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_2.pdf',
-        'noSolutionLink': 'nosolution3_2.pdf',
-        'powerpointLink': 'powerpoint3_2.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a13',
         'title': '3.3 حساب المشتقات: قاعدة القوة',
-        'name': '3.3 حساب المشتقات: قاعدة القوة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_3.pdf',
-        'noSolutionLink': 'nosolution3_3.pdf',
-        'powerpointLink': 'powerpoint3_3.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },    
     {
         'id': 'a14',
         'title': '3.4 قواعد الضرب والقسمة',
-        'name': '3.4 قواعد الضرب والقسمة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_4.pdf',
-        'noSolutionLink': 'nosolution3_4.pdf',
-        'powerpointLink': 'powerpoint3_4.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },    
     {
         'id': 'a15',
         'title': '3.5 قاعدة السلسلة',
-        'name': '3.5 قاعدة السلسلة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_5.pdf',
-        'noSolutionLink': 'nosolution3_5.pdf',
-        'powerpointLink': 'powerpoint3_5.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },    
     {
         'id': 'a16',
         'title': '3.6 مشتقات الدوال المثلثية',
-        'name': '3.6 مشتقات الدوال المثلثية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_6.pdf',
-        'noSolutionLink': 'nosolution3_6.pdf',
-        'powerpointLink': 'powerpoint3_6.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },    
     {
         'id': 'a17',
         'title': '3.7 اشتقاق الدوال الأسية والدوال اللوغاريتمية',
-        'name': '3.7 اشتقاق الدوال الأسية والدوال اللوغاريتمية.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_7.pdf',
-        'noSolutionLink': 'nosolution3_7.pdf',
-        'powerpointLink': 'powerpoint3_7.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a18',
         'title': '3.8 الاشتقاق الضمني والدوال المثلثية المعكوسة',
-        'name': '3.8 الاشتقاق الضمني والدوال المثلثية المعكوسة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_8.pdf',
-        'noSolutionLink': 'nosolution3_8.pdf',
-        'powerpointLink': 'powerpoint3_8.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a19',
         'title': '3.10 نظرية القيمة المتوسطة',
-        'name': '3.10 نظرية القيمة المتوسطة.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution3_10.pdf',
-        'noSolutionLink': 'nosolution3_10.pdf',
-        'powerpointLink': 'powerpoint3_10.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a21',
         'title': '4.1 التقريبات الخطية وطريقة نيوتن',
-        'name': '4.1 التقريبات الخطية وطريقة نيوتن.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution4_1.pdf',
-        'noSolutionLink': 'nosolution4_1.pdf',
-        'powerpointLink': 'powerpoint4_1.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
     {
         'id': 'a22',
         'title': '4.2 الصيغ غير المعرفة وقاعدة لوبيتال',
-        'name': '4.2 الصيغ غير المعرفة وقاعدة لوبيتال.mp4',
+        'name': '1Bn6WjJCVylub3-EzvAP-KtFSe5I3VEgo',
         'duration': '00:00',
-        'solutionLink': 'solution4_2.pdf',
-        'noSolutionLink': 'nosolution4_2.pdf',
-        'powerpointLink': 'powerpoint4_2.pdf',
+        'solutionLink': '',
+        'noSolutionLink': '',
+        'powerpointLink' : '',
     },
 ];
 
+
 data.forEach((video, i) => {
+    let links = '';
+    if (video.powerpointLink) {
+        links += `<a href="${video.powerpointLink}" target="_blank" class="powerpoint" style="color: var(--main-color);"> (powerpoint) </a>`;
+    }
+    if (video.solutionLink) {
+        links += video.noSolutionLink ? `<a href="${video.solutionLink}" target="_blank" class="solution" style="color: #57FF33;"> (solution) </a> <span> \xa0|\xa0 </span>` : '';
+    }
+    if (video.noSolutionLink) {
+        links += `<a href="${video.noSolutionLink}" target="_blank" class="no-solution" style="color: #FF5733;"> (no solution) </a>`;
+    }
     let video_element = `
-                <div class="video" data-id="${video.id}">
-                    <img src="play.svg" alt="">
-                    <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
-                    <h3 class="title">${video.title}</h3>
-                    ${video.powerpointLink ? `<a href="${video.powerpointLink}" target="_blank" class="powerpoint" style="color: var(--main-color);"> (powerpoint) </a>` : `<a href="${video.solutionLink}" target="_blank" class="solution" style="color: #57FF33;"> (solution) </a> <span> \xa0|\xa0 </span> <a href="${video.noSolutionLink}" target="_blank" class="no-solution" style="color: #FF5733;"> (no solution) </a>`}
-                    <p class="time">${video.duration}</p>
-                </div>
+      <div class="video" data-id="${video.id}">
+        <img src="play.svg" alt="">
+        <p>${i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
+        <h3 class="title">${video.title}</h3>
+        ${links ? `<p class="links">${links}</p>` : '<p class="links"></p>'}
+        <p class="time">${video.duration}</p>
+      </div>
     `;
     video_playlist.innerHTML += video_element;
 })
 
 let videos = document.querySelectorAll('.video');
-videos[0].classList.add('active');
-videos[0].querySelector('img').src = 'pause.svg';
 
+// إضافة حدث النقر على كل فيديو
 videos.forEach(selected_video => {
     selected_video.onclick = () => {
-
+        // إزالة الفيديو النشط من قائمة التشغيل
         for (all_videos of videos) {
             all_videos.classList.remove('active');
             all_videos.querySelector('img').src = 'play.svg';
-
         }
 
+        // إضافة الفيديو المحدد كفيديو نشط
         selected_video.classList.add('active');
         selected_video.querySelector('img').src = 'pause.svg';
 
+        // عرض الفيديو المحدد
         let match_video = data.find(video => video.id == selected_video.dataset.id);
-        main_video.src = '' + match_video.name;
+        let iframe_src = `https://drive.google.com/file/d/${match_video.name}/preview`;
+        let iframe_element = `<iframe src="${iframe_src}" frameborder="0" allowfullscreen sandbox="allow-scripts allow-same-origin"></iframe>`;
+        main_video.innerHTML = iframe_element;
         main_video_title.innerHTML = match_video.title;
     }
 });
+
+// إضافة حدث النقر على الفيديو المقدم (Introduction)
+let introduction_video = {
+    'id': 'introduction',
+    'title': 'Introduction',
+    'duration': '00:00',
+};
+
+let introduction_iframe_element = `
+    <iframe id="introduction" name="introduction" src="intro.mp4" frameborder="0" allowfullscreen allow="autoplay" sandbox="allow-scripts allow-same-origin nodownload"></iframe>
+`;
+
+main_video.innerHTML = introduction_iframe_element;
+main_video_title.innerHTML = introduction_video.title;
+
+// إزالة الفيديو النشط من قائمة التشغيل عند عرض الفيديو المقدم
+for (all_videos of videos) {
+    all_videos.classList.remove('active');
+    all_videos.querySelector('img').src = 'play.svg';
+}
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroLL sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar Links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar links (scroLL)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
 
 document.onkeydown = (e) => {
     if (e.key == 123) {
@@ -248,7 +330,6 @@ document.onkeydown = (e) => {
         e.preventDefault();
     }
 };
-
 const videoContainer = document.getElementById('video-container');
 const authenticatedUsers = [
   { email: 'a@z', password: 'a' }
